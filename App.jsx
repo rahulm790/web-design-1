dce4124ec95f5aaf173a84e2daaa80225ff48584
Rahul Mathpal <140446226+rahulm790@users.noreply.github.com>
2024-01-15 23:08:52 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
